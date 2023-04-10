20bc114956879668480968ab82b14ac1033aac7a
skm <fornewworld@yahoo.com>
2023-04-10 02:18:36 -0400

    410

---
+++ src/app/helpers/match.validator.ts
@@ -0,0 +1,20 @@
+import { FormGroup } from '@angular/forms';
+
+export function checkMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.checkMatch) {
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        console.log(control.value !== matchingControl.value);
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ checkMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    };
+}
